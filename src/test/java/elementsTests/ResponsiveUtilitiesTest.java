5e2f1e8ccc54329e59cc9a7248fdf364f87f759d
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-01-29 23:20:04 -0700

    test

--- src/test/java/elementsTests/ResponsiveUtilitiesTest.java
+++ src/test/java/elementsTests/ResponsiveUtilitiesTest.java
@@ -18,15 +18,17 @@ public class ResponsiveUtilitiesTest {
 	public WebDriver driver;
 	ResponsiveUtilitiesPageObjects respPgObj;
 	CommonUtils commonUtils;
-	public static final String USERNAME = "eajaz";
-	public static final String ACCESS_KEY = "dee10cb9-6c0f-4ce1-b3b4-ff7d2854ee80";
+	public static final String USERNAME = System.getenv("SAUCE_USERNAME");
+	public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
+	//public static final String USERNAME = "eajaz";
+	//public static final String ACCESS_KEY = "dee10cb9-6c0f-4ce1-b3b4-ff7d2854ee80";
 	public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
 
 	@BeforeTest
 	public void setUp() throws MalformedURLException{
 
 		DesiredCapabilities caps = DesiredCapabilities.chrome();
-		//caps.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
+		caps.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
 		caps.setCapability("platform", "OSX 10.8");
 		caps.setCapability("version", "43.0");
 		System.out.println("TRAVIS_JOB_NUMBER: "+System.getenv("TRAVIS_JOB_NUMBER"));				
@@ -40,7 +42,6 @@ public class ResponsiveUtilitiesTest {
 	public void xtraSmallTest() throws InterruptedException {
 
 		String inputFilePath = "src/main/java/elements/fixtures/responsive.html";
-		System.out.println(Paths.get(inputFilePath));
 		String url = new File(inputFilePath).getAbsolutePath();
 		Thread.sleep(5000);
 		commonUtils.getUrl("http://localhost:8000/src/main/java/elements/fixtures/responsive.html"); 
