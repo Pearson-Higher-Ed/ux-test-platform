1e2e325f0ed3b844db72450e19f852ab2fa16b32
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-04 15:42:26 -0700

    add base class

---
+++ src/test/java/elementsTests/BaseClass.java
@@ -0,0 +1,80 @@
+package elementsTests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import elements.elementsPageObjects.ResponsiveUtilitiesPageObjects;
+import elements.utilities.CommonUtils;
+import org.testng.annotations.Parameters;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by umahaea on 2/3/16.
+ */
+public class BaseClass {
+	
+	public static WebDriver driver;
+	public static ResponsiveUtilitiesPageObjects respPgObj;
+	public static CommonUtils commonUtils;
+	
+	String USERNAME="eajaz";
+	String ACCESS_KEY="dee10cb9-6c0f-4ce1-b3b4-ff7d2854ee80";
+
+    @Parameters({"runEnv","travis","platform","vmBrowser","vmBrowserVer","localBrowser"})
+	@BeforeSuite
+	protected void setUp(String runEnv, String travis, String platform, String vmBrowser, String vmBrowserVer,String localBrowser) throws MalformedURLException{
+    	System.out.println("entering before suite");
+    	System.out.println(runEnv.equals("sauce"));	
+		if(runEnv.equals("sauce")) {
+
+            DesiredCapabilities caps = new DesiredCapabilities();
+
+			/*** The below conditions is to launch the respective browser driver on Sauce machine ***/
+            if(vmBrowser.equals("chrome")) {
+                caps = DesiredCapabilities.chrome();
+            }
+            else if (vmBrowser.equals("firefox")) {
+                caps = DesiredCapabilities.firefox();
+            }
+            else if (vmBrowser.equals("ie")) {
+                caps = DesiredCapabilities.internetExplorer();
+            }
+
+            /*** The below condition will work only when the build is triggered through Travis CI and in testng.xml -> set travis to 'on' ***/
+            if(travis.equals("on")) {
+            	USERNAME = System.getenv("SAUCE_USERNAME");
+    			ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
+    			caps.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
+                caps.setCapability("build", System.getenv("TRAVIS_BUILD_NUMBER"));
+			}
+
+            final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+            caps.setCapability("platform", platform);
+            caps.setCapability("version", vmBrowserVer);
+            driver = new RemoteWebDriver(new URL(URL), caps);
+		}
+        /*** The below else condition is to lauch browser driver on your local machine. In testng.xml -> set runEnv != sauce ***/
+		else {
+			System.out.println(localBrowser.equals("firefox"));
+            if(localBrowser.equals("firefox")) {
+                driver = new FirefoxDriver();
+            }
+		}
+		respPgObj = new ResponsiveUtilitiesPageObjects(driver);
+		commonUtils = new CommonUtils(driver);
+		System.out.println("exiting before suite");
+    }
+    
+	@AfterSuite
+	public void tearDown() {
+		System.out.println("entering after suite");
+		driver.close();
+		driver.quit();
+		System.out.println("exiting after suite");
+	}
+}
--- src/test/java/elementsTests/ResponsiveUtilitiesTest.java
+++ src/test/java/elementsTests/ResponsiveUtilitiesTest.java
@@ -1,59 +1,28 @@
 package elementsTests;
 
-import elements.elementsPageObjects.ResponsiveUtilitiesPageObjects;
-import elements.utilities.CommonUtils;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Paths;
 
-public class ResponsiveUtilitiesTest {
+public class ResponsiveUtilitiesTest extends BaseClass {
 
-	public WebDriver driver;
-	ResponsiveUtilitiesPageObjects respPgObj;
-	CommonUtils commonUtils;
-	public static final String USERNAME = System.getenv("SAUCE_USERNAME");
-	public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
-	//public static final String USERNAME = "eajaz";
-	//public static final String ACCESS_KEY = "dee10cb9-6c0f-4ce1-b3b4-ff7d2854ee80";
-	public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
-
-	@BeforeTest
-	public void setUp() throws MalformedURLException{
-
-		DesiredCapabilities caps = DesiredCapabilities.chrome();
-		caps.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
-		caps.setCapability("build", System.getenv("TRAVIS_BUILD_NUMBER"));
-		caps.setCapability("platform", "OSX 10.8");
-		caps.setCapability("version", "43.0"); 
-		driver = new RemoteWebDriver(new URL(URL), caps);
-		//driver = new FirefoxDriver();
-		respPgObj = new ResponsiveUtilitiesPageObjects(driver);
-		commonUtils = new CommonUtils(driver);
-	}
-
+	@Parameters({ "runEnv" })
 	@Test(testName = "xtraSmallResponsiveTest")
-	public void xtraSmallTest() throws InterruptedException {
+	public void xtraSmallTest(String runEnv) {
 
-		String inputFilePath = "src/main/java/elements/fixtures/responsive.html";
-		String url = new File(inputFilePath).getAbsolutePath();
-		Thread.sleep(5000);
-		commonUtils.getUrl("http://localhost:8000/src/main/java/elements/fixtures/responsive.html"); 
-		commonUtils.setWindowSize(300,800);
-
+		if (runEnv.equals("sauce")) {
+			commonUtils.getUrl("http://localhost:8000/src/main/java/elements/fixtures/responsive.html");
+		}
+        else {
+			String inputFilePath = "src/main/java/elements/fixtures/responsive.html";
+			String url = new File(inputFilePath).getAbsolutePath();
+			commonUtils.getUrl("file:///" + url);
+		}
+		commonUtils.setWindowSize(300, 800);
 		String actual = commonUtils.getCSSValue(respPgObj.xtraSmall,"background-color");
-		Assert.assertEquals(actual, "rgba(255, 0, 0, 0)");
+		Assert.assertEquals(actual, "rgba(255, 0, 0, 1)");
 	}
 
-	@AfterTest
-	public void tearDown() {
-		driver.close();
-		driver.quit();
-	}
 }
