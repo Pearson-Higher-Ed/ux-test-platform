91966e84789bf70fab11d4eef13fe946818c935e
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-08 17:55:33 -0700

    feat: Add mobile tests for responsive utilities

--- src/main/java/elements/elementsPageObjects/ResponsiveUtilitiesPageObjects.java
+++ src/main/java/elements/elementsPageObjects/ResponsiveUtilitiesPageObjects.java
@@ -1,4 +1,6 @@
 package elements.elementsPageObjects;
+
+import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -8,19 +10,24 @@ import org.openqa.selenium.WebDriver;
 public class ResponsiveUtilitiesPageObjects {
 
     public WebDriver driver;
+    public AppiumDriver appium;
 
-    public ResponsiveUtilitiesPageObjects(WebDriver driver){
+    public ResponsiveUtilitiesPageObjects(WebDriver driver) {
         this.driver = driver;
     }
 
-    public By xtraSmallVisible=By.id("xs-visible");
-    public By xtraSmallHidden=By.id("xs-hidden");
-    public By smallVisible=By.id("sm-visible");
-    public By smallHidden=By.id("sm-hidden");
-    public By mediumVisible=By.id("md-visible");
-    public By mediumHidden=By.id("md-hidden");
-    public By largeVisible=By.id("lg-visible");
-    public By largeHidden=By.id("lg-hidden");
-    public By xtraLargeVisible=By.id("xl-visible");
-    public By xtraLargeHidden=By.id("xl-hidden");
+    public ResponsiveUtilitiesPageObjects(AppiumDriver appium) {
+        this.appium = appium;
+    }
+
+    public By xtraSmallVisible = By.id("xs-visible");
+    public By xtraSmallHidden = By.id("xs-hidden");
+    public By smallVisible = By.id("sm-visible");
+    public By smallHidden = By.id("sm-hidden");
+    public By mediumVisible = By.id("md-visible");
+    public By mediumHidden = By.id("md-hidden");
+    public By largeVisible = By.id("lg-visible");
+    public By largeHidden = By.id("lg-hidden");
+    public By xtraLargeVisible = By.id("xl-visible");
+    public By xtraLargeHidden = By.id("xl-hidden");
 }
