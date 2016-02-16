95596920d9ddca4b196b71101fbab3a03d65365b
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-16 14:42:00 -0700

    desktop tests for icons

---
+++ src/test/java/elementsTests/IconsTest.java
@@ -0,0 +1,80 @@
+package elementsTests;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by umahaea on 2/15/16.
+ */
+public class IconsTest extends BaseClass {
+
+    private final String url = "http://localhost:8000/src/main/java/elements/fixtures/icons.html";
+    private String inputFilePath = "src/main/java/elements/fixtures/icons.html";
+    private String localUrl = new File(inputFilePath).getAbsolutePath();
+    private static String env;
+    private static String mobileDevice;
+    String fetchCharacter;
+    String content;
+
+    @Parameters({"runEnv", "mobDeviceName"})
+    @BeforeClass(alwaysRun = true)
+    private void iconsTestBeforeClass(String runEnv, String mobDeviceName) {
+        env = runEnv;
+        mobileDevice = mobDeviceName;
+    }
+
+    @DataProvider(name = "getIconsTestData")
+    private Object[][] getIconsTestData() {
+        return new Object[][]{
+                {"check", "\\f00c"},
+                {"chevron-down", "\\f078"},
+                {"chevron-up", "\\f077"},
+                {"chevron-right", "\\f058"},
+                {"chevron-left", "\\f056"},
+                {"cog", "\\f013"},
+                {"envelope", "\\f0e0"},
+                {"plus-circle", "\\f055"},
+                {"search", "\\f002"},
+                {"thumb-tack", "\\f08d"},
+                {"times", "\\f00d"},
+                {"times-circle", "\\f057"},
+                {"trash-o", "\\f014"},
+                {"users", "\\f0c0"},
+                {"info-circle", "\\f05a"},
+                {"user", "\\f007"},
+                {"file-o", "\\f016"},
+                {"calendar", "\\f073"}
+        };
+    }
+
+    @Test(testName = "Icons Test", dataProvider = "getIconsTestData", groups = {"desktop"})
+    private void iconsTest(String icon, String expectedContent) throws InterruptedException, UnsupportedEncodingException {
+        chooseEnv();
+        fetchCharacter = "return window.getComputedStyle(document.querySelector('.pe-icon--" + icon + "'), ':before').getPropertyValue('content')";
+        String actualContent = getCode(fetchCharacter);
+        Assert.assertEquals(actualContent, expectedContent, "The icon " + icon + " is not as per the SPEC");
+    }
+
+    private String getCode(String script) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        content = (String) js.executeScript(script);
+        String t = StringEscapeUtils.escapeJava(content);
+        return "\\" + t.substring(4, 8).toLowerCase();
+    }
+
+    private void chooseEnv() {
+        if (env.equals("sauce")) {
+            commonUtils.getUrl(url);
+        } else {
+            commonUtils.getUrl("file:///" + localUrl);
+        }
+    }
+}
--- src/test/java/elementsTests/ResponsiveUtilitiesTest.java
+++ src/test/java/elementsTests/ResponsiveUtilitiesTest.java
@@ -1,9 +1,7 @@
 package elementsTests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Rotatable;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.*;
@@ -12,23 +10,22 @@ import java.io.File;
 
 public class ResponsiveUtilitiesTest extends BaseClass {
 
-    final String url = "http://localhost:8000/src/main/java/elements/fixtures/responsive.html";
-    String inputFilePath = "src/main/java/elements/fixtures/responsive.html";
-    String localUrl = new File(inputFilePath).getAbsolutePath();
-    String responsiveValue_1, responsiveValue_2;
-    String landscapeValue_1, landscapeValue_2, portraitValue_1, portraitValue_2;
-    static String env;
-    static String mobileDevice;
+    private final String url = "http://localhost:8000/src/main/java/elements/fixtures/responsive.html";
+    private String inputFilePath = "src/main/java/elements/fixtures/responsive.html";
+    private String localUrl = new File(inputFilePath).getAbsolutePath();
+    private String responsiveValue_1, responsiveValue_2;    
+    private static String env;
+    private static String mobileDevice;
 
     @Parameters({"runEnv", "mobDeviceName"})
     @BeforeClass(alwaysRun = true)
-    public void beforeClass(String runEnv, String mobDeviceName) {
+    private  void beforeClass(String runEnv, String mobDeviceName) {
         env = runEnv;
         mobileDevice = mobDeviceName;
     }
 
     @DataProvider(name = "LGTestData")
-    public Object[][] getLGTestData() {
+    private  Object[][] getLGTestData() {
         return new Object[][]{
                 {1024, 800, respPgObj.largeVisible, "lg-visible", "rgba(139, 0, 139, 1)"},
                 {1279, 800, respPgObj.largeVisible, "lg-visible", "rgba(139, 0, 139, 1)"},
@@ -38,13 +35,13 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "LG Test", dataProvider = "LGTestData", groups = {"desktop"})
-    public void lgTest(int width, int height, By element, String visible, String color) throws InterruptedException {
+    private  void lgTest(int width, int height, By element, String visible, String color) throws InterruptedException {
         chooseEnv();
         performRespEval(width, height, element, visible, color);
     }
 
     @DataProvider(name = "XLTestData")
-    public Object[][] getXLTestData() {
+    private  Object[][] getXLTestData() {
         return new Object[][]{
                 {1280, 800, respPgObj.xtraLargeVisible, "xl-visible", "rgba(173, 255, 47, 1)"},
                 {1279, 800, respPgObj.xtraLargeHidden, "xl-hidden", "rgba(128, 128, 128, 1)"},
@@ -52,13 +49,13 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "XL Test", dataProvider = "XLTestData", groups = {"desktop"})
-    public void xlTest(int width, int height, By element, String visible, String color) {
+    private  void xlTest(int width, int height, By element, String visible, String color) {
         chooseEnv();
         performRespEval(width, height, element, visible, color);
     }
 
     @DataProvider(name = "XSTestData")
-    public Object[][] getXSTestData() {
+    private  Object[][] getXSTestData() {
         return new Object[][]{
                 {479, 800, respPgObj.xtraSmallVisible, "xs-visible", "rgba(255, 0, 0, 1)"},
                 {480, 800, respPgObj.xtraSmallHidden, "xs-hidden", "rgba(128, 128, 128, 1)"},
@@ -66,13 +63,13 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "XS Test", dataProvider = "XSTestData", groups = {"desktop"})
-    public void xsTest(int width, int height, By element, String visible, String color) {
+    private  void xsTest(int width, int height, By element, String visible, String color) {
         chooseEnv();
         performRespEval(width, height, element, visible, color);
     }
 
     @DataProvider(name = "SMTestData")
-    public Object[][] getSMTestData() {
+    private Object[][] getSMTestData() {
         return new Object[][]{
                 {480, 800, respPgObj.smallVisible, "sm-visible", "rgba(0, 128, 0, 1)"},
                 {767, 800, respPgObj.smallVisible, "sm-visible", "rgba(0, 128, 0, 1)"},
@@ -82,13 +79,13 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "SM Test", dataProvider = "SMTestData", groups = {"desktop"})
-    public void smTest(int width, int height, By element, String visible, String color) {
+    private void smTest(int width, int height, By element, String visible, String color) {
         chooseEnv();
         performRespEval(width, height, element, visible, color);
     }
 
     @DataProvider(name = "MDTestData")
-    public Object[][] getMDTestData() {
+    private Object[][] getMDTestData() {
         return new Object[][]{
                 {768, 800, respPgObj.mediumVisible, "md-visible", "rgba(0, 0, 255, 1)"},
                 {1023, 800, respPgObj.mediumVisible, "md-visible", "rgba(0, 0, 255, 1)"},
@@ -98,12 +95,12 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "MD Test", dataProvider = "MDTestData", groups = {"desktop"})
-    public void mdTest(int width, int height, By element, String visible, String color) {
+    private void mdTest(int width, int height, By element, String visible, String color) {
         chooseEnv();
         performRespEval(width, height, element, visible, color);
     }
 
-    public void chooseEnv() {
+    private void chooseEnv() {
         if (env.equals("sauce")) {
             commonUtils.getUrl(url);
         } else {
@@ -111,7 +108,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
         }
     }
 
-    public void performRespEval(int width, int height, By element, String visible, String color) {
+    private void performRespEval(int width, int height, By element, String visible, String color) {
         commonUtils.setWindowSize(width, height);
         responsiveValue_1 = commonUtils.getText(element);
         responsiveValue_2 = commonUtils.getCSSValue(element, "color");
@@ -119,7 +116,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
         Assert.assertEquals(responsiveValue_2, color, "Responsive Failed");
     }
 
-    public void performRespForMobileEval(By element, String visible, String color) {
+    private void performRespForMobileEval(By element, String visible, String color) {
         responsiveValue_1 = commonUtils.getText(element, "mobile");
         System.out.println("actual: " + responsiveValue_1 + " --- expected: " + visible);
         responsiveValue_2 = commonUtils.getCSSValue(element, "color", "mobile");
@@ -132,7 +129,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     
     //For iPhone 6 Plus
     @DataProvider(name = "iPhone6PlusTestData")
-    public Object[][] iPhone6PlusTestData() {
+    private Object[][] iPhone6PlusTestData() {
         return new Object[][]{
                 {ScreenOrientation.PORTRAIT, respPgObj.xtraSmallVisible, "xs-visible", "rgba(255, 0, 0, 1)"},
                 {ScreenOrientation.LANDSCAPE, respPgObj.xtraSmallHidden, "xs-hidden", "rgba(128, 128, 128, 1)"},
@@ -142,7 +139,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "iPhone 6 Plus", dataProvider = "iPhone6PlusTestData", groups = {"mobile"})
-    public void iPhone6PlusResponsiveTest(ScreenOrientation mode, By element, String visible, String color) {
+    private void iPhone6PlusResponsiveTest(ScreenOrientation mode, By element, String visible, String color) {
         if (!(mobileDevice.equals("iPhone 6 Plus"))) {
             throw new SkipException("To run this test specify mobile device as 'iPhone 6 Plus'");
         }
@@ -153,7 +150,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
 
     //For iPad Air
     @DataProvider(name = "iPadAirTestData")
-    public Object[][] iPadAirTestData() {
+    private Object[][] iPadAirTestData() {
         return new Object[][]{
                 {ScreenOrientation.PORTRAIT, respPgObj.mediumVisible, "md-visible", "rgba(0, 0, 255, 1)"},
                 {ScreenOrientation.LANDSCAPE, respPgObj.mediumHidden, "md-hidden", "rgba(128, 128, 128, 1)"},
@@ -163,7 +160,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "iPad Air", dataProvider = "iPadAirTestData", groups = {"mobile"})
-    public void iPadAirResponsiveTest(ScreenOrientation mode, By element, String visible, String color) {
+    private void iPadAirResponsiveTest(ScreenOrientation mode, By element, String visible, String color) {
         if (!(mobileDevice.equals("iPad Air"))) {
             throw new SkipException("To run this test specify mobile device as 'iPad Air'");
         }
@@ -174,7 +171,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
 
     //iPad Pro
     @DataProvider(name = "iPadProTestData")
-    public Object[][] iPadProTestData() {
+    private Object[][] iPadProTestData() {
         return new Object[][]{
                 {ScreenOrientation.PORTRAIT, respPgObj.largeVisible, "lg-visible", "rgba(139, 0, 139, 1)"},
                 {ScreenOrientation.LANDSCAPE, respPgObj.xtraLargeVisible, "xl-visible", "rgba(173, 255, 47, 1)"},
@@ -182,7 +179,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "iPad Pro", dataProvider = "iPadProTestData", groups = {"mobile"})
-    public void iPadProResponsiveTest(ScreenOrientation mode, By element, String visible, String color) {
+    private void iPadProResponsiveTest(ScreenOrientation mode, By element, String visible, String color) {
         if (!(mobileDevice.equals("iPad Pro"))) {
             throw new SkipException("To run this test specify mobile device as 'iPad Pro'");
         }
@@ -193,7 +190,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
 
     //Nexus 7
     @DataProvider(name = "nexus7TestData")
-    public Object[][] nexus7TestData() {
+    private Object[][] nexus7TestData() {
         return new Object[][]{
                 {ScreenOrientation.PORTRAIT, respPgObj.smallVisible, "sm-visible", "rgba(0, 128, 0, 1)"},
                 {ScreenOrientation.LANDSCAPE, respPgObj.mediumVisible, "md-visible", "rgba(0, 0, 255, 1)"},
@@ -201,7 +198,7 @@ public class ResponsiveUtilitiesTest extends BaseClass {
     }
 
     @Test(testName = "nexus7", dataProvider = "nexus7TestData", groups = {"mobile"})
-    public void nexus7ResponsiveTest(ScreenOrientation mode, By element, String visible, String color) {
+    private void nexus7ResponsiveTest(ScreenOrientation mode, By element, String visible, String color) {
         if (!(mobileDevice.equals("Google Nexus 7 HD Emulator"))) {
             throw new SkipException("To run this test specify mobile device as 'Google Nexus 7 HD Emulator'");
         }
