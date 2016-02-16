e57450300daafdbf17212cb3cddc30ed30016707
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-16 15:20:31 -0700

    ie run

--- src/test/java/elementsTests/BaseClass.java
+++ src/test/java/elementsTests/BaseClass.java
@@ -52,7 +52,7 @@ public class BaseClass {
                     caps = DesiredCapabilities.chrome();
                 } else if (vmBrowser.equals("firefox")) {
                     caps = DesiredCapabilities.firefox();
-                } else if (vmBrowser.equals("ie")) {
+                } else if (vmBrowser.equals("internet explorer")) {
                     caps = DesiredCapabilities.internetExplorer();
                 }
                 caps.setCapability("platform", platform);
