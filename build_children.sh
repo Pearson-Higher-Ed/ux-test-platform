ad3c55e136d8f61d118b9cfd1f719aa46cdf085a
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-22 12:24:30 -0700

    test

--- build_children.sh
+++ build_children.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Memory information"
-free -m
-echo "Disk information"
-df -h
+
+# Get last child project build number
+BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/Pearson-Higher-Ed/travis-test/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+echo $BUILD_NUM
+# Restart last child project build
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
