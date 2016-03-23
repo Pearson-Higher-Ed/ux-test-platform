8a5ae8cb9e99f44b6c1426d2eff37304cf38851b
Viraji Paranavitana <viraji.paranavitana@pearson.com>
2016-03-23 14:52:46 +0530

    List and Body tests

---
+++ src/main/java/elements/elementsPageObjects/BodyPageObjects.java
@@ -0,0 +1,23 @@
+package elements.elementsPageObjects;
+
+import io.appium.java_client.AppiumDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class BodyPageObjects {
+    
+	public WebDriver driver;
+    public AppiumDriver appium;
+
+    public BodyPageObjects(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public BodyPageObjects(AppiumDriver appium) {
+        this.appium = appium;
+    }
+
+    public By bodyElement = By.id("basic-body");
+
+}
---
+++ src/main/java/elements/elementsPageObjects/ListsPageObjects.java
@@ -0,0 +1,36 @@
+package elements.elementsPageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import io.appium.java_client.AppiumDriver;
+
+public class ListsPageObjects {
+
+	public WebDriver driver;
+    public AppiumDriver appium;
+
+    public ListsPageObjects(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public ListsPageObjects(AppiumDriver appium) {
+        this.appium = appium;
+    }
+
+    public By orderedList=By.xpath("/html/body/div[1]/ol");
+    public By orderedListItem1 = By.xpath("/html/body/div[1]/ol/li[1]");
+    public By orderedListItem2 = By.xpath("/html/body/div[1]/ol/li[2]"); 
+    public By orderedListItem3 = By.xpath("/html/body/div[1]/ol/li[3]");
+    public By orderedListChildItem1 = By.xpath("/html/body/div[1]/ol/ol/li[1]");
+    public By orderedListChildItem2 = By.xpath("/html/body/div[1]/ol/ol/li[2]"); 
+    public By orderedListGrandChildItem1 = By.xpath("/html/body/div[1]/ol/ol/ol/li"); 
+    
+    public By unorderedList=By.xpath("/html/body/div[2]/ul");
+    public By unorderedListItem1 = By.xpath("/html/body/div[2]/ul/li[1]");
+    public By unorderedListItem2 = By.xpath("/html/body/div[2]/ul/li[2]"); 
+    public By unorderedListItem3 = By.xpath("/html/body/div[2]/ul/li[3]");
+    public By unorderedListChildItem1 = By.xpath("/html/body/div[2]/ul/ul/li[1]");
+    public By unorderedListChildItem2 = By.xpath("/html/body/div[2]/ul/ul/li[2]"); 
+    public By unorderedListGrandChildItem1 = By.xpath("/html/body/div[2]/ul/ul/ul/li"); 
+}
