ad2887dd0fb2178041c5bebc970f7c82d9d8a2ca
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-09-30 16:19:44 -0600

    Regression - speedup

--- src/main/shell_scripts/components.sh
+++ src/main/shell_scripts/components.sh
@@ -2,10 +2,12 @@
 
 echo "component: $component"
 echo "feature_branch: $feature_branch"
+grn=$'\e[1;32m'
 
 install_elements_sdk() {
 echo -e "******************************\\n    Installing elements sdk V1   \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/elements.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/elements.git &>/dev/null
 cd elements
 git checkout $1
 npm install &>/dev/null
@@ -14,26 +16,29 @@ cp ~/build/Pearson-Higher-Ed/ux-test-platform/elements/dist/css/elements.css ~/b
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/elements/dist/css/elementsNoPlain.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/elements/css/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/elements/dist/icons ~/build/Pearson-Higher-Ed/ux-test-platform/
+printf "${grn}EndOf elements_sdk Installation\n"
 }
 
 install_compounds_sdk() {
 echo -e "******************************\\n    Installing compounds sdk V0   \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/compounds.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/compounds.git &>/dev/null
 cd compounds
 git checkout $1
 npm install &>/dev/null
-#npm run copy-utils
 npm run build &>/dev/null
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/compounds/build/dist.compounds.js ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/compounds/jsfiles/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/compounds/build/eventInstantiator.compounds.js ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/compounds/jsfiles/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/compounds/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/compounds/node_modules/pearson-elements/dist/icons ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/compounds/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/compounds/css/
+printf "${grn}EndOf compounds_sdk Installation\n"
 }
 
 install_appHeader(){
 echo -e "******************************\\n    Installing app-header V2  \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/app-header.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/app-header.git &>/dev/null
 cd app-header
 git checkout $1
 npm install &>/dev/null
@@ -42,11 +47,13 @@ cp ~/build/Pearson-Higher-Ed/ux-test-platform/app-header/build/dist.app-header.j
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/app-header/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/app-header/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/appHeader/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/app-header/images ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/
+printf "${grn}EndOf app-header Installation\n"
 }
 
 install_contextualHelp(){
 echo -e "******************************\\n    Installing contextual-help V2   \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/contextual-help.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/contextual-help.git &>/dev/null
 cd contextual-help
 git checkout $1
 npm install &>/dev/null
@@ -55,34 +62,39 @@ cp ~/build/Pearson-Higher-Ed/ux-test-platform/contextual-help/build/dist.context
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/contextual-help/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/contextual-help/node_modules/pearson-elements/dist/icons ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/contextual-help/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/contextualHelp/
+printf "${grn}EndOf contextual-help Installation\n"
 }
 
 install_drawer(){
 echo -e "******************************\\n    Installing drawer    \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/drawer.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/drawer.git &>/dev/null
 cd drawer
 git checkout $1
 npm install &>/dev/null
 npm run build &>/dev/null
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/drawer/build/dist.drawer.js ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/jsfiles/drawer/
+printf "${grn}EndOf drawer Installation\n"
 }
 
 install_componentArchetype(){
 echo -e "******************************\\n    Installing component-archetype    \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/component-archetype.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/component-archetype.git &>/dev/null
 cd component-archetype
 git checkout $1
 npm install &>/dev/null
-npm run copy-utils
 npm run build &>/dev/null
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/component-archetype/build/dist.component-name.js ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/jsfiles/componentArchetype/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/component-archetype/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/component-archetype/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/componentArchetype/
+printf "${grn}EndOf component-archetype Installation\n"
 }
 
 install_avatarDisplay(){
 echo -e "******************************\\n    Installing avatar-display    \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/avatar-display.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/avatar-display.git &>/dev/null
 cd avatar-display
 git checkout $1
 npm install &>/dev/null
@@ -91,11 +103,13 @@ npm run build &>/dev/null
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/avatar-display/build/dist.avatar-display.js ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/jsfiles/avatarDisplay/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/avatar-display/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/avatar-display/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/avatarDisplay/
+printf "${grn}EndOf avatar-display Installation\n"
 }
 
 install_slider(){
 echo -e "******************************\\n    Installing slider    \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/slider.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/slider.git &>/dev/null
 cd slider
 git checkout $1
 npm install &>/dev/null
@@ -104,11 +118,13 @@ npm run build &>/dev/null
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/slider/build/dist.slider.js ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/jsfiles/slider/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/slider/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/slider/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/slider/slider.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/slider/
+printf "${grn}EndOf slider Installation\n"
 }
 
 install_textModal(){
 echo -e "******************************\\n    Installing text-modal    \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/text-modal.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/text-modal.git &>/dev/null
 cd text-modal
 git checkout $1
 npm install &>/dev/null
@@ -117,11 +133,13 @@ npm run build &>/dev/null
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/text-modal/build/dist.text-modal.js ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/jsfiles/textModal/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/text-modal/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/text-modal/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/textModal/
+printf "${grn}EndOf text-modal Installation\n"
 }
 
 install_alerts(){
 echo -e "******************************\\n    Installing alerts    \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/alerts.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/alerts.git &>/dev/null
 cd alerts
 git checkout $1
 npm install &>/dev/null
@@ -131,11 +149,13 @@ cp ~/build/Pearson-Higher-Ed/ux-test-platform/alerts/build/dist.alerts.js ~/buil
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/alerts/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/alerts/node_modules/pearson-elements/dist/icons ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/alerts/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/alerts/
+printf "${grn}EndOf alerts Installation\n"
 }
 
 install_pagination(){
 echo -e "******************************\\n    Installing pagination    \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/pagination.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/pagination.git &>/dev/null
 cd pagination
 git checkout $1
 npm install &>/dev/null
@@ -144,11 +164,13 @@ npm run build &>/dev/null
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/pagination/build/dist.pagination.js ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/jsfiles/pagination/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/pagination/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/pagination/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/pagination/
+printf "${grn}EndOf pagination Installation\n"
 }
 
 install_modal(){
 echo -e "******************************\\n    Installing modal    \\n******************************"
-git clone https://github.com/Pearson-Higher-Ed/modal.git
+cd ~/build/Pearson-Higher-Ed/ux-test-platform/
+git clone https://github.com/Pearson-Higher-Ed/modal.git &>/dev/null
 cd modal
 git checkout $1
 npm install &>/dev/null
@@ -158,6 +180,7 @@ cp ~/build/Pearson-Higher-Ed/ux-test-platform/modal/build/dev.modal.js ~/build/P
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/modal/node_modules/pearson-elements/dist/fonts ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/modal/node_modules/pearson-elements/dist/icons ~/build/Pearson-Higher-Ed/ux-test-platform/
 cp ~/build/Pearson-Higher-Ed/ux-test-platform/modal/node_modules/pearson-elements/dist/css/elements.css ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/origamiV2/css/modal/
+printf "${grn}EndOf modal Installation\n"
 }
 
 # Below conditions are to install the components specific to its feature branch.
@@ -214,27 +237,17 @@ install_modal $feature_branch
 # Below condition is to install all the "master" branch of components for the regression test run
 elif [[ $component == "regression" ]]
 then
-install_appHeader master
-cd ..
-install_contextualHelp master
-cd ..
-install_drawer master
-cd ..
-install_componentArchetype master
-cd ..
-install_avatarDisplay master
-cd ..
-install_slider master
-cd ..
-install_textModal master
-cd ..
-install_alerts master
-cd ..
-install_pagination master
-cd ..
-install_modal master
-cd ..
-install_elements_sdk v1
-cd ..
-install_compounds_sdk v0
+install_appHeader master &
+install_contextualHelp master &
+install_drawer master &
+install_avatarDisplay master &
+install_slider master &
+install_textModal master &
+install_alerts master &
+install_pagination master &
+install_modal master &
+install_elements_sdk v1 &
+install_compounds_sdk v0 &
 fi
+jobs -l
+wait
