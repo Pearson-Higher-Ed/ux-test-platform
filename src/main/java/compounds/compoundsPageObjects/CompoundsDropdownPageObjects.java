4902fda1e3980a5381581ada03e1c0707ea83962
udhadpa <payal.dhadge@pearson.com>
2017-09-14 12:34:07 -0700

    feat(tables):Fix dropdown page object

--- src/main/java/compounds/compoundsPageObjects/CompoundsDropdownPageObjects.java
+++ src/main/java/compounds/compoundsPageObjects/CompoundsDropdownPageObjects.java
@@ -24,8 +24,10 @@ public class CompoundsDropdownPageObjects {
     public By textDropdownItem3 = By.xpath("(.//*[@id='text-dropdown']/li[@role='presentation'])[3]");
     public By textDropdownDivider = By.xpath(".//*[@id='text-dropdown']/li[@class='divider-container']");
 
-    public By iconDropdownActivator = By.xpath("//button[@class='dropdown-activator']");
-    public By iconBtnSvg = By.xpath("//button[@class='dropdown-activator']//*[local-name() = 'svg']");
+    //public By iconDropdownActivator = By.xpath("//button[@class='dropdown-activator']");
+    //public By iconBtnSvg = By.xpath("//button[@class='dropdown-activator']//*[local-name() = 'svg']");
+    public By iconDropdownActivator = By.xpath("//*[@id='dropdown-target']/div/button");
+    public By iconBtnSvg = By.cssSelector("#dropdown-target > div > button > svg");
 
     public By buttonDropdown = By.xpath(".//*[@class='pe-btn dropdown-activator']");
     public By buttonText = By.xpath(".//*[@class='pe-btn dropdown-activator']/div");
