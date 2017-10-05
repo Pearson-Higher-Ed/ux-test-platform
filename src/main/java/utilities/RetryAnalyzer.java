7b8c3e1adf972f91f8a3fe01325a66bdec346e19
Mahaboob Basha, Eajaz Ali <EajazAli.MahaboobBasha@pearson.com>
2017-10-05 09:37:22 -0600

    increment retry by 1

--- src/main/java/utilities/RetryAnalyzer.java
+++ src/main/java/utilities/RetryAnalyzer.java
@@ -9,7 +9,7 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     private int count = 0;
-    private static int maxTry = 2;
+    private static int maxTry = 3;
 
     @Override
     public boolean retry(ITestResult iTestResult) {
