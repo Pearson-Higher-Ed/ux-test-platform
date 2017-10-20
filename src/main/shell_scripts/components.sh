ce9d4edbdd8a3af1698db2b49e0ef298cef89826
Mahaboob Basha, Eajaz Ali <EajazAli.MahaboobBasha@pearson.com>
2017-10-20 11:45:06 -0600

    test cov on travis

--- src/main/shell_scripts/components.sh
+++ src/main/shell_scripts/components.sh
@@ -182,8 +182,8 @@ cp ~/build/Pearson-Higher-Ed/ux-test-platform/loading-indicator/node_modules/pea
 }
 
 instrument_file(){
-js-beautify ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/standAlone/jsfiles/$1/$2.js
-nyc instrument ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/standAlone/jsfiles/$1/$2.js >> ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/standAlone/jsfiles/$1/$2-instrumented.js &>/dev/null
+js-beautify ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/standAlone/jsfiles/$1/$2.js >> ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/standAlone/jsfiles/$1/$2.js
+nyc instrument ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/standAlone/jsfiles/$1/$2.js >> ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/standAlone/jsfiles/$1/$2-instrumented.js
 ls -ltr ~/build/Pearson-Higher-Ed/ux-test-platform/src/main/java/standAlone/jsfiles/$1
 }
 
