522a00ea3f42266a692037c8df945dcc21ddc668
Mahaboob Basha, Eajaz Ali <EajazAli.MahaboobBasha@pearson.com>
2017-10-19 21:02:42 -0600

    test cov on travis

--- src/main/shell_scripts/components.sh
+++ src/main/shell_scripts/components.sh
@@ -62,9 +62,9 @@ cp -R ~/build/Pearson-Higher-Ed/ux-test-platform/app-header/images ~/build/Pears
 }
 
 instrument_file(){
-- js-beautify --version
+"js-beautify --version"
 js-beautify ~/build/Pearson-Higher-Ed/ux-test-platform/$1/build/$2 >> ~/build/Pearson-Higher-Ed/ux-test-platform/$1/build/$2
-- nyc --version
+nyc --version
 nyc instument ~/build/Pearson-Higher-Ed/ux-test-platform/$1/build/$2 >> ~/build/Pearson-Higher-Ed/ux-test-platform/$1/build/$2
 }
 
