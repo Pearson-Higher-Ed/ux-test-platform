c75c79c7b45ed88edebe8beb3500e2747a40e620
udhadpa <payal.dhadge@pearson.com>
2018-04-24 12:04:05 -0700

    Con-help config

--- src/main/java/standAlone/jsfiles/contextualHelp/contextual-help.js
+++ src/main/java/standAlone/jsfiles/contextualHelp/contextual-help.js
@@ -1,31 +1,92 @@
+let conHelpProps = {
+    directTopic: undefined,
+    drawerTop: "61px",
+    handleHelp: function () {},
+    //defaultLanguage: 'en-us',
+    intl: {
+        locale: 'fr'
+    },
+    showHelp: false,
+    text: {
+        backButton: 'Back',
+        closeButton: 'Close',
+        headerTitle: 'FRENCH'
+    },
+    topics: [
+        'console/student/freetrial',
+        'pi/forgot_creds/next',
+        'contactsupport'
+    ]
+}
+
 function init() {
     document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
         detail: {
             elementId: 'contextual-help-target',
-            props: {
-                directTopic: undefined,
-                drawerTop: "61px",
-                handleHelp: function () {
-                },
-                language: 'fr',
-                showHelp: true,
-                text: {
-                    backButton: 'Back',
-                    closeButton: 'Close',
-                    headerTitle: 'Help Topics'
-                },
-                topics: [
-                    'console/student/freetrial',
-                    //'pi/forgot_creds/next',
-                    'contactsupport',
-                    'console/instructor/courseregsettings',
-                    'custom/text/demo/example'
-                ]
-            }
+            props: conHelpProps
         }
     }));
+
+    const helpIndexButton = document.createElement('button');
+    helpIndexButton.innerText = 'Help Index';
+    helpIndexButton.onclick = function() {
+        conHelpProps.directTopic = undefined;
+        conHelpProps.showHelp = true;
+        conHelpProps.handleHelp = function() {
+            conHelpProps.directTopic = undefined;
+            conHelpProps.showHelp = false;
+            document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
+                detail: {
+                    elementId: 'contextual-help-target',
+                    props: conHelpProps
+                }
+            }));
+        };
+        document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
+            detail: {
+                elementId: 'contextual-help-target',
+                props: conHelpProps
+            }
+        }));
+    };
+
+    const jumpToTopicButton = document.createElement('button');
+    jumpToTopicButton.innerText = 'Direct Topic';
+    jumpToTopicButton.onclick = function() {
+        conHelpProps.directTopic = 'contactsupport';
+        conHelpProps.showHelp = true;
+        conHelpProps.handleHelp = function() {
+            conHelpProps.showHelp = false;
+            document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
+                detail: {
+                    elementId: 'contextual-help-target',
+                    props: conHelpProps
+                }
+            }));
+            setTimeout(function() {
+                conHelpProps.directTopic = undefined;
+                document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
+                    detail: {
+                        elementId: 'contextual-help-target',
+                        props: conHelpProps
+                    }
+                }));
+            }, 500);
+        };
+        document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
+            detail: {
+                elementId: 'contextual-help-target',
+                props: conHelpProps
+            }
+        }));
+    }
+
+    document.body.appendChild(jumpToTopicButton);
+    document.body.appendChild(helpIndexButton);
 }
+
 window.onload = init;
+
 
 
 /*
@@ -34,7 +95,7 @@ let conHelpProps = {
     drawerTop: "61px",
     handleHelp: function () {},
     language: 'en-us',
-    showHelp: false,
+    showHelp: true,
     text: {
         backButton: 'Back',
         closeButton: 'Close',
@@ -42,17 +103,36 @@ let conHelpProps = {
     },
     topics: [
         'console/student/freetrial',
-        'pi/forgot_creds/next',
-        'contactsupport'
+        //'pi/forgot_creds/next',
+        'contactsupport',
+        'console/instructor/courseregsettings'
     ]
 }
+
 function init() {
-    document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
-        detail: {
-            elementId: 'contextual-help-target',
-            props: conHelpProps
+    const helpIndexButton = document.createElement('button');
+    helpIndexButton.innerText = 'Help Index';
+    helpIndexButton.onclick = function() {
+        conHelpProps.directTopic = undefined;
+        conHelpProps.showHelp = true;
+        conHelpProps.handleHelp = () => {
+            conHelpProps.directTopic = undefined;
+            conHelpProps.showHelp = false;
+            document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
+                detail: {
+                    elementId: 'contextual-help-target',
+                    props: conHelpProps
+                }
+            }));
         }
-    }));
+        document.body.dispatchEvent(new CustomEvent('o.InitComponent', {
+            detail: {
+                elementId: 'contextual-help-target',
+                props: conHelpProps
+            }
+        }));
+    };
+
     const jumpToTopicButton = document.createElement('button');
     jumpToTopicButton.innerText = 'Direct Topic';
     jumpToTopicButton.onclick = function() {
@@ -75,7 +155,9 @@ function init() {
             }
         }));
     }
+
     document.body.appendChild(jumpToTopicButton);
+    document.body.appendChild(helpIndexButton);
 }
-window.onload = init;
-*/
+
+window.onload = init;*/
